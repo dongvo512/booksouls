b1671b70d08ab2f78a2555ed95ff109343118e5c
Dong Vo <dongvo512@gmail.com>
2017-10-16 16:08:21 +0700

    [commit][create app]

---
+++ BookSouls/BookSouls/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BookSouls
+//
+//  Created by Dong Vo on 10/16/17.
+//  Copyright © 2017 Dong Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
